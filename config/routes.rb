5a1b6dc49d11998788cc2b277b81a47ef0d98586
Jules Wei <s.jules2000@gmail.com>
2016-03-28 21:08:59 +0000

    added post page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
-Rails.application.routes.draw do
+LighthouseForum::Application.routes.draw do
+    get 'posts', to: 'posts#index'
+    #[tons of helpful comments]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
