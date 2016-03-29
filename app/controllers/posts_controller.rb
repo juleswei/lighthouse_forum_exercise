3c2896560659dbec1d2a894af14bfb5bb297e277
Jules Wei <s.jules2000@gmail.com>
2016-03-29 15:10:19 +0000

    added show and edit pages and changes routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,92 @@
 class PostsController < ApplicationController
 
-  # def index
-  def post0
-    @post = {
-        title: "Superstar",
-        author: "Carly Rae Jepson",
-        text: <<-eos.gsub(/\s+/, " ").strip
-          Khurram Virani has been my music idol since I started writing
-          songs back when I was 4. His voice is a revelation. His stage presence
-          is unparalleled. And those costumes! He remains an inspiration to this
-          day.
-        eos
-      }
+  def index
+    @posts = Post.all
   end
 
-  def post1
-    @post = {
-        title: "Basketball Idol",
-        author: "Steve Nash",
-        text: <<-eos.gsub(/\s+/, " ").strip
-          I remember watching Khurram Virani (#14) play back when he just playing
-          pickup games on the street. Dude had moves nobody had ever seen. Breaking
-          ankles. Poppin' threes. Great all-around game.
-        eos
-      }
+  def show
+    @post = Post.find(params[:id])
   end
 
-  def post2
-    @post = {
-        title: "Acting Legend",
-        author: "Michael J. Fox",
-        text: <<-eos.gsub(/\s+/, " ").strip
-          Back when I was in university, Khurram Virani was my acting coach. He
-          studied with the best and it shows. His acting chops were already legendary
-          before his teaching career began. But it seems he's actually improved!
-        eos
-      }
+  def new
+    @post = Post.new
   end
 
-  def post3 
-    @post = {
-        title: "Who?",
-        author: "Vurram Khirani",
-        text: "Never heard of this guy Khurram Virani, but he sounds great."
-      }
+  def create
+    @post = Post.new(
+      title:  params[:post][:title],
+      author: params[:post][:author],
+      text:   params[:post][:text]
+    )
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end  
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
+
+  # def show
+  #   posts = [
+  #     {
+  #       title: "Superstar",
+  #       author: "Carly Rae Jepson",
+  #       text: <<-eos.gsub(/\s+/, " ").strip
+  #         Khurram Virani has been my music idol since I started writing
+  #         songs back when I was 4. His voice is a revelation. His stage presence
+  #         is unparalleled. And those costumes! He remains an inspiration to this
+  #         day.
+  #       eos
+  #     },
+  #     {
+  #       title: "Basketball Idol",
+  #       author: "Steve Nash",
+  #       text: <<-eos.gsub(/\s+/, " ").strip
+  #         I remember watching Khurram Virani (#14) play back when he just playing
+  #         pickup games on the street. Dude had moves nobody had ever seen. Breaking
+  #         ankles. Poppin' threes. Great all-around game.
+  #       eos
+  #     },
+  #     {
+  #       title: "Acting Legend",
+  #       author: "Michael J. Fox",
+  #       text: <<-eos.gsub(/\s+/, " ").strip
+  #         Back when I was in university, Khurram Virani was my acting coach. He
+  #         studied with the best and it shows. His acting chops were already legendary
+  #         before his teaching career began. But it seems he's actually improved!
+  #       eos
+  #     },
+  #     {
+  #       title: "Who?",
+  #       author: "Vurram Khirani",
+  #       text: "Never heard of this guy Khurram Virani, but he sounds great."
+  #     }
+  #   ]
+  #   @post = posts[params[:id].to_i]
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 LighthouseForum::Application.routes.draw do
-    get 'posts', to: 'posts#index'
 
-    get 'posts/0', to: 'posts#post0'
-    get 'posts/1', to: 'posts#post1'
-    get 'posts/2', to: 'posts#post2'
-    get 'posts/3', to: 'posts#post3'
+    # get 'posts', to: 'posts#index'
+    # get 'posts/new', to: 'posts#new'
+    # get 'posts/:id/edit', to: 'posts#edit'
+    # get 'posts/:id', to: 'posts#show', as: 'post' #necessary for the update action!
+    # patch 'posts/:id', to: 'posts#update'
+    # post 'posts', to: 'posts#create'
+    resources :posts
+    root to: 'posts#index'
+
 
     #[tons of helpful comments]
   # The priority is based upon order of creation: first created -> highest priority.
